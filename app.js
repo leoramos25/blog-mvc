511fa49543e0b655aa7de47de5fa09b81caf4567
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-06-11 18:28:23 -0300

    header added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/', router);
 app.use(express.json());
 
 //Mustache settings
-app.engine('mst', mustache());
+app.engine('mst', mustache(__dirname + '/views/partials', '.mst'));
 app.set('view engine', 'mst');
 app.set('views', __dirname + '/views');
 
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,29 @@ const express = require('express');
 const router = express.Router();
 router.get('/', (req, res) => {
     let data = {
-        'name':'Leonardo',
-        'age':22,
-    }
-    
-    res.render('home');
+        name: req.query.name,
+        surname: req.query.surname,
+        open:true,
+        studies:[
+            {topic:'Javascript', hours:2},
+            {topic:'NodeJS', hours:2},
+            {topic:'C#', hours:2},
+            {topic:'ReactJS', hours:1},
+            {topic:'SQL', hours:0.5},
+            {topic:'NoSQL',hours:0.5},
+        ],
+        objectives: [
+            'JavaScript',
+            'NodeJS',
+            'C#',
+            'ReactJS',
+            'React Native',
+            'SQL',
+            'NoSQL'
+        ],
+    }; 
+
+    res.render('home', data);
 });
 
 module.exports = router;
