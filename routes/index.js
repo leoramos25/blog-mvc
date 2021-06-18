aadc4e049018344c598819d0fc4ccf0116ac7678
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-06-18 13:59:14 -0300

    upload images

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const homeController = require('../controllers/homeController');
 const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
+const imageMiddleware = require('../middlewares/imageMiddleware');
 
 const router = express.Router();
 router.get('/', homeController.index);
@@ -10,7 +11,11 @@ router.get('/login', userController.login);
 router.get('/register', userController.register);
 
 router.get('/post/add', postController.add);
-router.post('/post/add', postController.addAction);
+router.post('/post/add',
+    imageMiddleware.upload,
+    imageMiddleware.resize,
+    postController.addAction,
+);
 
 router.get('/post/:slug/edit', postController.edit);
 router.post('/post/:slug/edit', postController.editAction);
