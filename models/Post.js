ad3be0b80af2a9ad3ecb00ca962d7b523e419c11
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-07-06 15:57:15 -0300

    Post signature

--- models/Post.js
+++ models/Post.js
@@ -2,40 +2,61 @@ const mongoose = require('mongoose');
 const slug = require('slug');
 mongoose.Promise = global.Promise;
 
+const objectId = mongoose.Schema.Types.ObjectId;
+
 const postSchema = new mongoose.Schema({
-    photo:String,
-    title:{
-        type:String,
-        trim:true,
-        required:'Título é obrigatório',
+    photo: String,
+    title: {
+        type: String,
+        trim: true,
+        required: 'Título é obrigatório',
     },
-    slug:String,
-    body:{
-        type:String,
-        trim:true
+    slug: String,
+    body: {
+        type: String,
+        trim: true
     },
-    tags:[String],
+    tags: [String],
+    author: objectId,
 });
 
-postSchema.pre('save', async function(next) {
-    if(this.isModified('title')) {
-        this.slug = slug(this.title, {lower:true});
+postSchema.pre('save', async function (next) {
+    if (this.isModified('title')) {
+        this.slug = slug(this.title, { lower: true });
 
         const slugRegex = new RegExp(`^(${this.slug})((-[0-9]{1,}$)?)$`, 'i');
-        const postsWithSlug = await this.constructor.find({slug:slugRegex});
+        const postsWithSlug = await this.constructor.find({ slug: slugRegex });
 
-        if(postsWithSlug.length > 0) {
+        if (postsWithSlug.length > 0) {
             this.slug = `${this.slug}-${postsWithSlug.length + 1}`;
         };
     };
     next();
 });
 
-postSchema.statics.getTagsList = function() {
+postSchema.statics.getTagsList = function () {
     return this.aggregate([
-        {$unwind:'$tags'},
-        {$group:{_id:'$tags', count:{$sum:1}}},
-        {$sort:{count: -1, _id: 1}},
+        { $unwind: '$tags' },
+        { $group: { _id: '$tags', count: { $sum: 1 } } },
+        { $sort: { count: -1, _id: 1 } },
+    ]);
+};
+
+postSchema.statics.findPosts = function (filters = { }) {
+    return this.aggregate([
+        { $match: filters },
+        { $lookup: {
+            from: 'users',
+            let: { 'author': '$author' },
+            pipeline: [
+                { $match: { $expr: { $eq: [ '$$author', '$_id' ] } } },
+                { $limit: 1 }
+            ],
+            as: 'author',
+        } },
+        { $addFields:{
+            'author':{ $arrayElemAt: [ '$author', 0 ] }
+        } }
     ]);
 };
 
